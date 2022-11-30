d5a0033079cb56b1c7624b68056f951ee29bbdec
Anna Kozlova <annakozlova@MacBook-Air-Anna.local>
2022-11-30 22:47:29 +0200

    add week7 code

---
+++ Functions/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Are you coming or leaving? (in/out):");
+string userChoice = Console.ReadLine();
+
+if(userChoice == "in")
+{
+    PrintHello();
+}
+else
+{
+    PrintGoodbue();
+}
+
+
+static void PrintHello() //function / method
+{
+ Console.WriteLine("Hello, World!");
+}
+
+
+static void PrintGoodbue()
+{
+ Console.WriteLine("See you later, aligator.");
+}
